bc709559d74b85270c10205d4606f98e7dc8ab4a
RoaryStar <hanssun07@gmail.com>
2017-11-25 14:27:04 -0500

    setup training

--- hack/main.cpp
+++ hack/main.cpp
@@ -7,15 +7,13 @@ const double rand_mult = 1.0/RAND_MAX;
 
 int main()
 {
-    srand(time(NULL));
-    matrix a(2,1);
-    a[0][0] = 1;
-    a[1][0] = 2;
-    matrix b(2,2);
-    b[0][0] = 1;
-    b[0][1] = 2;
-    b[1][0] = 3;
-    b[1][1] = 4;
-    matrix_mult(b,a).print();
+    nn net(2,7);
+    net.randomize();
+
+    //infrastucture and read data
+
+
+    //train network
+
     return 0;
 }
